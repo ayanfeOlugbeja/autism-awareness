744fa9c4d9cd52c485b8da20bcd434b091ed151c
iOWERETH <ayanfeolugbeja@gmail.com>
2024-11-29 10:25:56 +0100

    support-section

--- src/Components/Support.jsx
+++ src/Components/Support.jsx
@@ -1,88 +1,173 @@
-import React from 'react';
-import image from './../assets/Tonya-and-Tyler.png';
+import info1 from './../assets/information-and-support-1.png';
+import info2 from './../assets/information-and-support-2.png';
+import info3 from './../assets/information-and-support-3.png';
+import services1 from './../assets/services-and-event-1.png';
+import services2 from './../assets/services-and-events-2.png';
+import services3 from './../assets/services-and-events-3.png';
+import resources1 from './../assets/resources-1.png';
+import resources2 from './../assets/resources-2.png';
+import resources3 from './../assets/resources-3.png';
+
 function Support() {
   return (
-    <div className='w-full h-[1600px] flex flex-col justify-between items-center p-10'>
-      <div className='h-1/3 '>
-        <h2 className='text-gray-500 font-bold text-3xl '>
+    <div className='w-full flex flex-col gap-16 p-6 md:p-10'>
+      {/* Autism Information and Support Section */}
+      <section className='flex flex-col items-center gap-10'>
+        <h2 className='text-gray-500 font-bold text-3xl text-center'>
           Autism information and support
         </h2>
-        {/* <div className='grid gap-8 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 px-6'>
+        <div className='grid gap-8 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 w-full'>
           {[
             {
-              title: 'Autism Awareness Africa',
-              image: image,
-              alt: 'Autism Awareness Africa website',
-              repo: 'https://github.com/ayanfeOlugbeja',
+              title: 'What is autism?',
+              image: info1,
+              alt: 'Learn about autism spectrum disorder.',
+              description: 'Learn more about autism spectrum disorder.',
             },
             {
-              title: 'Aim ProDrive Training Centre',
-              image: image,
-              alt: 'Aim Prodrive Training Centre booking website',
-              repo: 'https://github.com/ayanfeOlugbeja/aim-pro',
+              title: 'Signs of autism',
+              image: info2,
+              alt: 'Learn the signs of autism at any age.',
+              description: 'Learn the signs of autism at any age.',
             },
             {
-              title: 'PASSI (Exeat/Pass System)',
-              image: image,
-              alt: 'Pass/Exeat application system',
-              repo: 'https://github.com/ayanfeOlugbeja/exeat-app',
+              title: 'Newly diagnosed',
+              image: info3,
+              alt: 'Get resources and support after a new autism diagnosis.',
+              description:
+                'Get resources and support after a new autism diagnosis.',
             },
-          ].map((information, index) => (
+          ].map((info, index) => (
             <div
               key={index}
-              className='bg-white shadow-lg rounded-lg overflow-hidden dark:bg-gray-800 dark:border-gray-700 transition-transform transform hover:scale-105'>
-              <div className='relative group'>
+              className='bg-white shadow-lg overflow-hidden h-80 dark:bg-gray-800 dark:border-gray-700'>
+              <div className='relative'>
                 <img
-                  className='w-full h-48 object-contain'
-                  src={information.image}
-                  alt={information.alt}
+                  className='w-full h-56 object-cover'
+                  src={info.image}
+                  alt={info.alt}
+                  loading='lazy'
                 />
-                <div className='absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex justify-center items-center'>
-                  <a
-                    href={information.website}
-                    target='_blank'
-                    rel='noopener noreferrer'
-                    className='text-white text-lg font-semibold py-2 px-6 border border-white rounded-full hover:bg-white hover:text-black transition-colors duration-300'>
-                    Visit Website
-                  </a>
+                <div className='absolute inset-0 flex items-center justify-center bg-black bg-opacity-50'>
+                  <h3 className='text-blue-500 text-lg text-center bg-white p-2 h-10 w-3/4 hover:underline self-end'>
+                    {info.title}
+                  </h3>
                 </div>
               </div>
-              <div className='p-6'>
-                <h5 className='text-gray-900 font-bold text-xl mb-3 dark:text-white'>
-                  {information.title}
-                </h5>
-                <p className='text-gray-700 dark:text-gray-400 mb-4 text-sm'>
-                  {information.description}
+              <div className='p-4'>
+                <p className='text-gray-700 dark:text-gray-400 text-center'>
+                  {info.description}
                 </p>
-                <a
-                  href={information.repo}
-                  target='_blank'
-                  rel='noopener noreferrer'
-                  className='inline-flex items-center'>
-                  <img
-                    className='h-6 w-6 mr-2'
-                    src={github}
-                    alt='GitHub repository'
-                  />
-                  <span className='text-blue-600 hover:underline dark:text-blue-400'>
-                    View Code
-                  </span>
-                </a>
               </div>
             </div>
           ))}
-        </div> */}
-      </div>
-      <div className='h-1/3 '>
-        <h2 className='text-gray-500 font-bold text-3xl '>
+        </div>
+      </section>
+
+      {/* Autism Services and Events Section */}
+      <section className='flex flex-col items-center gap-10'>
+        <h2 className='text-gray-500 font-bold text-3xl text-center'>
           Find autism services and events in your community
         </h2>
-      </div>
-      <div className='h-1/3'>
-        <h2 className='text-gray-500 font-bold text-3xl '>
+        <div className='grid gap-8 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 w-full'>
+          {[
+            {
+              title: 'Local services',
+              image: services1,
+              alt: 'Discover autism support services near you.',
+              description: 'Discover autism support services near you.',
+            },
+            {
+              title: 'Community events',
+              image: services2,
+              alt: 'Join autism awareness and support events.',
+              description: 'Join autism awareness and support events.',
+            },
+            {
+              title: 'Support groups',
+              image: services3,
+              alt: 'Connect with autism support groups in your community.',
+              description:
+                'Connect with autism support groups in your community.',
+            },
+          ].map((services, index) => (
+            <div
+              key={index}
+              className='bg-white shadow-lg overflow-hidden h-80 dark:bg-gray-800 dark:border-gray-700'>
+              <div className='relative'>
+                <img
+                  className='w-full h-56 object-cover'
+                  src={services.image}
+                  alt={services.alt}
+                  loading='lazy'
+                />
+                <div className='absolute inset-0 flex items-center justify-center bg-black bg-opacity-50'>
+                  <h3 className='text-blue-500 text-lg text-center bg-white p-2 h-10 w-3/4 hover:underline self-end'>
+                    {services.title}
+                  </h3>
+                </div>
+              </div>
+              <div className='p-4'>
+                <p className='text-gray-700 dark:text-gray-400 text-center'>
+                  {services.description}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+
+      {/* Resources Section */}
+      <section className='flex flex-col items-center gap-10'>
+        <h2 className='text-gray-500 font-bold text-3xl text-center'>
           Find the resources right for you
         </h2>
-      </div>
+        <div className='grid gap-8 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 w-full'>
+          {[
+            {
+              title: 'Educational resources',
+              image: resources1,
+              alt: 'Access educational materials about autism.',
+              description: 'Access educational materials about autism.',
+            },
+            {
+              title: 'Parent guides',
+              image: resources2,
+              alt: 'Explore parenting guides for autism support.',
+              description: 'Explore parenting guides for autism support.',
+            },
+            {
+              title: 'Professional support',
+              image: resources3,
+              alt: 'Find professional resources for autism care.',
+              description: 'Find professional resources for autism care.',
+            },
+          ].map((resources, index) => (
+            <div
+              key={index}
+              className='bg-white shadow-lg overflow-hidden h-80 dark:bg-gray-800 dark:border-gray-700'>
+              <div className='relative'>
+                <img
+                  className='w-full h-56 object-cover'
+                  src={resources.image}
+                  alt={resources.alt}
+                  loading='lazy'
+                />
+                <div className='absolute inset-0 flex items-center justify-center bg-black bg-opacity-50'>
+                  <h3 className='text-blue-500 text-lg text-center bg-white p-2 h-10 w-3/4 hover:underline self-end'>
+                    {resources.title}
+                  </h3>
+                </div>
+              </div>
+              <div className='p-4'>
+                <p className='text-gray-700 dark:text-gray-400 text-center'>
+                  {resources.description}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
     </div>
   );
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,14 +1,23 @@
 import Support from '../Components/Support';
 import heroImage from './../assets/Tonya-and-Tyler.png';
+
 function Home() {
   return (
     <div>
-      <div className='mt-32 h-[950px] w-full  bg-gradient-to-r from-pink-500 to-red-500 p-10 flex flex-col justify-around'>
-        <h1 className='text-white font-bold text-4xl w-11/12 '>
+      {/* Hero Section */}
+      <div className='mt-32 h-auto w-full bg-gradient-to-r from-pink-500 to-red-500 p-6 md:p-10 flex flex-col md:flex-row items-center md:justify-around gap-8'>
+        <h1 className='text-white font-bold text-2xl md:text-4xl text-center md:text-left w-full md:w-1/2'>
           Making strides today toward a spectrum of possibilities tomorrow.
         </h1>
-        <img src={heroImage} alt='Tonya-and-Tyler-hero' />
+        <img
+          className='w-full md:w-1/2 h-auto object-contain'
+          src={heroImage}
+          alt='Tonya and Tyler hero image showcasing autism awareness'
+          loading='lazy'
+        />
       </div>
+
+      {/* Support Section */}
       <Support />
     </div>
   );
