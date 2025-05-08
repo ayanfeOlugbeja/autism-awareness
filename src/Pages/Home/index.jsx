c3e4122bd9104765bf5fd569bfe9bd5090c49919
Aiyedogbon Abraham Ayodeji AO <ayanfeolugbeja@gmail.com>
2025-05-08 20:13:06 +0100

    Update index.jsx

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+
+import {  useNavigate } from 'react-router-dom'
 import { Container } from '../../components/layout'
-import { Heading, BodyText, SmallText } from '../../components/typography'
+import { Heading, BodyText} from '../../components/typography'
 import { Button } from '../../components/interactive'
 import { QuickLink } from '../../components/interactive'
 
