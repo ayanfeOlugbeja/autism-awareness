7b2c64b1ff0bff6f7b8f0a225ea16292eb708097
Aiyedogbon Abraham Ayodeji AO <ayanfeolugbeja@gmail.com>
2025-05-08 20:25:27 +0100

    Update index.jsx

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,7 +1,6 @@
 import { useNavigate } from 'react-router-dom'
 import { Container } from '../../components/layout'
 import { Heading, BodyText } from '../../components/typography'
-import { Heading, BodyText } from '../../components/typography'
 import { Button } from '../../components/interactive'
 import { QuickLink } from '../../components/interactive'
 
